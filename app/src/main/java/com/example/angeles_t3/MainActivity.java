515735f1871c1d9dd37d49ad6e7f77242ed3d4e4
Carlos Angeles <33170529+CyberLas@users.noreply.github.com>
2020-11-06 07:51:39 -0500

    Base del Proyecto

---
+++ app/src/main/java/com/example/angeles_t3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.angeles_t3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
